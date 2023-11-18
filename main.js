4f6aefdaa762101dccaeb08bbb65a94bbda5931d
Harishkumar <harishkumarsp023@gmail.com>
2023-11-18 07:11:58 +0530

    internal server error handling added

--- main.js
+++ main.js
@@ -5,11 +5,22 @@ const db = require('./data/database');
 const quoteroutes = require('./routes/quotes.routes');
 
 app.use('/quots',quoteroutes);
-app.use(function(error,req,res,next){
+app.use(function(error, req, res, next) {
+    console.error(error.stack); 
+
     res.status(500).json({
-        message:'some thing went wrong'
-    })
-})
+        error: 'Internal Server Error',
+        message: error.message || 'Something went wrong',
+    });
+});
+app.use((req, res, next) => {
+    res.status(404).json({
+        error: 'Not Found',
+        message: 'The requested resource was not found on this server.',
+    });
+});
+
+
 db.initDb().then(function(){
     app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
